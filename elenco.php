467235b9e3e7edbbce1bd91e77324f5166afc853
montepagano <rachele.montepagano@villaggio.org>
2017-02-08 17:25:15 +0100

    Add files via upload

--- elenco.php
+++ elenco.php
@@ -10,10 +10,13 @@
 	$record=(empty($_REQUEST['id'])) ?  R::dispense($table) : R::load($table, intval($_REQUEST['id']));	
 	try {
 		if ($record && !empty($_REQUEST['act']) && $_REQUEST['act']=='del') R::trash($record);
+                
+                
                 $new = json_decode(file_get_contents('php://input'), true);
 		if (!empty($new)){       
 			foreach ($new as $k=>$v){
 				$record[$k]=$v;
+                                
 			}
                         R::store($record);
                }
@@ -21,5 +24,8 @@
 		
 	}	
 	$pa=R::find($table);
+        
+        
         echo json_encode($pa);
+ 
 ?>
--- temperatura.php
+++ temperatura.php
@@ -1,25 +1,42 @@
 <?php
-    error_reporting(E_ALL);
-        ini_set("display_errors", 1);  
+
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+//REF: http://www.redbeanphp.com/index.php
+require_once('lib/rb.php');
+R::setup('mysql:host=127.0.0.1;dbname=pa', 'pa', 'pressione');
+$error = "";
+$table = 'temperatura';
+$record = (empty($_REQUEST['id'])) ? R::dispense($table) : R::load($table, intval($_REQUEST['id']));
+try {
+    if ($record && !empty($_REQUEST['act']) && $_REQUEST['act'] == 'del')
+        R::trash($record);
+    $new = json_decode(file_get_contents('php://input'), true);
+    if (!empty($new)) {
+
+        if ($new["valore"] < 30) {
+            $error .= " errore perche e' troppo bassa e sei morto...AHAH";
+        } else if ($new["valore"] > 42) {
+            $error .= " errore perche e' troppo alta e sei morto...AHAH";
+        }
         
-	//REF: http://www.redbeanphp.com/index.php
-	require_once('lib/rb.php');
-	R::setup( 'mysql:host=127.0.0.1;dbname=pa','pa', 'pressione' );
-        
-	$table='temperatura';
-	$record=(empty($_REQUEST['id'])) ?  R::dispense($table) : R::load($table, intval($_REQUEST['id']));	
-	try {
-		if ($record && !empty($_REQUEST['act']) && $_REQUEST['act']=='del') R::trash($record);
-                $new = json_decode(file_get_contents('php://input'), true);
-		if (!empty($new)){       
-			foreach ($new as $k=>$v){
-				$record[$k]=$v;
-			}
-                        R::store($record);
-               }
-	} catch (RedBeanPHP\RedException\SQL $e) {
-		
-	}	
-	$pa=R::find($table);
-        echo json_encode($pa);
+
+
+        if (!$error) {
+            foreach ($new as $k => $v) {
+                $record[$k] = $v;
+            }
+            R::store($record);
+        }
+    }
+} catch (RedBeanPHP\RedException\SQL $e) {
+    
+}
+$pa = R::find($table);
+$result = ["error" => $error, "list" => $pa];
+//die(print_r($result,1));
+$json = json_encode($result, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_NUMERIC_CHECK);
+//die(print_r($json, 1) );
+die($json);
 ?>
